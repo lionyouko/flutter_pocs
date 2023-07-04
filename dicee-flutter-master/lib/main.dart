b358d6b9a12dfbf7f23a0ffc0c5bc2fd12a9c25e
lionyouko <lionyouko91@yahoo.com.br>
2023-07-04 08:53:04 +0100

    flutter stateful widgets

--- dicee-flutter-master/lib/main.dart
+++ dicee-flutter-master/lib/main.dart
@@ -15,18 +15,61 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+// Mutable
+/**
+ * Stateful Widgets are mutable.
+ * They need to override a method called createState that is triggered once the widget is inflated and inserted in Widgets tree.
+ * 
+ * that Method os of type State<OurWidgetClass> and it will evoke the creation of a class
+ * that we need to implement (OurWidgetClass) and its parent is State<OurWidgetClass>.
+ * 
+ * Inside State<OurWidgetClass> we can set variables.
+ * 
+ * It will contain the build method where we put our UI code just as Stateless Widget.
+ * 
+ * Since it is stateful, when we reload or rebuild, it should change what was changed (variables values, for example). 
+ * It does change, however, it may not appear in the screen. For that, we need to mark the things that changed in some way and that way is by the use of setState() method. 
+ * 
+ * When setState is called, all inside of it will be set as "dirty" when when a reload is done. Then, the build method will have a elements market as dirty, meaning they must be updated to the most recent value. Then it does that on rebuild.
+ * 
+ */
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int _leftDiceNumber = 1;
+  int _rightDiceNumber = 2;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice2.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  _leftDiceNumber = 5;
+                });
+              },
+              child: Image.asset('images/dice$_leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  _rightDiceNumber = 3;
+                });
+              },
+              child: Image.asset('images/dice$_rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
