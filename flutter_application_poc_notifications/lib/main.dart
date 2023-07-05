150967acc83864b0eaec654a612b703f0cbd4934
lionyouko <lionyouko91@yahoo.com.br>
2023-07-05 05:18:14 +0100

    poc notification dialog and toast

--- flutter_application_poc_notifications/lib/main.dart
+++ flutter_application_poc_notifications/lib/main.dart
@@ -13,22 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Notification PoC',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 58, 116, 183)),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 58, 116, 183)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Notification PoC Home Page'),
@@ -39,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -56,70 +33,246 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Map<String, bool> _stateMap = {};
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+      if (_counter % 5 == 0) {
+        print('hey');
+        _notifyUserWhenMultipleOfFive(context);
+      }
     });
+  }
+
+  void _listenSwitchStateChanges(Map<String, bool> stateMap) {
+    _stateMap = stateMap;
+    print(_stateMap);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SwitchesGroup(onSwitchStateChange: this._listenSwitchStateChanges),
+          const SizedBox(
+            width: 100,
+            height: 50,
+          ),
+          const Text(
+            'Push + button. For every 5 times, notification will pop.',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _notifyUserWhenMultipleOfFive(BuildContext context) {
+    for (String key in _stateMap.keys) {
+      if (_stateMap[key]!) {
+        _invokeNotification(type: key, context: context);
+      }
+    }
+  }
+
+  void _invokeNotification(
+      {required String type, required BuildContext context}) {
+    switch (type) {
+      case 'DIALOG':
+        {
+          showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('Brushing Time!'),
+              content: const Text('Time To Brush Your Teeth'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'Cancel'),
+                  child: const Text(
+                    'Later',
+                    selectionColor: Colors.red,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'OK'),
+                  child: const Text(
+                    'Let\'s Go',
+                    selectionColor: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+        break;
+      case 'TOAST':
+        {
+          final mySnackBar = SnackBar(
+            content: const Text('You made it! Keep Going'),
+            action: SnackBarAction(
+              label: 'Close',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(mySnackBar);
+        }
+        break;
+      case 'LOCALPUSH':
+        {}
+    }
+  }
+}
+
+class SwitchesGroup extends StatefulWidget {
+  final void Function(Map<String, bool> stateMap) onSwitchStateChange;
+
+  const SwitchesGroup({super.key, required this.onSwitchStateChange});
+
+  @override
+  State<SwitchesGroup> createState() => _SwitchesGroupState();
+}
+
+class _SwitchesGroupState extends State<SwitchesGroup> {
+  Map<String, bool> _switchesStateMap = {
+    'DIALOG': false,
+    'TOAST': false,
+    'LOCALPUSH': false
+  };
+
+/*   
+  bool _switchDialogEnabled = false;
+  bool _SwitchToastEnabled = false;
+  bool _switchLocalPushEnabled = false; 
+*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            _createASwitch(state: _switchesStateMap['DIALOG']!, type: 'DIALOG'),
+            const Text('DIALOG'),
+          ],
+        ),
+        Row(
+          children: [
+            _createASwitch(state: _switchesStateMap['TOAST']!, type: 'TOAST'),
+            const Text('TOAST'),
+          ],
+        ),
+        Row(
+          children: [
+            _createASwitch(
+                state: _switchesStateMap['LOCALPUSH']!, type: 'LOCALPUSH'),
+            const Text('LOCALPUSH'),
+          ],
+        ),
+      ],
+    );
+  }
+
+  bool getSwitchState({String? type}) {
+    bool stateGotten;
+
+    switch (type) {
+      case 'DIALOG':
+        stateGotten = _switchesStateMap['DIALOG']!;
+        break;
+      case 'TOAST':
+        stateGotten = _switchesStateMap['TOAST']!;
+        break;
+      case 'LOCALPUSH':
+        stateGotten = _switchesStateMap['LOCALPUSH']!;
+        break;
+      default:
+        stateGotten = false;
+    }
+
+    return stateGotten;
+  }
+
+  Switch _createASwitch({bool state = false, String? type}) {
+    return Switch(
+      value: state,
+      activeColor: Colors.green,
+      onChanged: (value) {
+        setState(() {
+          if (value) {
+            switch (type) {
+              case 'DIALOG':
+                {
+                  // _switchDialogEnabled = true;
+                  _switchesStateMap['DIALOG'] = true;
+                  // _SwitchToastEnabled = false;
+                  _switchesStateMap['TOAST'] = false;
+                  //  _switchLocalPushEnabled = false;
+                  _switchesStateMap['LOCALPUSH'] = false;
+                }
+                break;
+              case 'TOAST':
+                {
+                  // _switchDialogEnabled = false;
+                  _switchesStateMap['DIALOG'] = false;
+                  //  _SwitchToastEnabled = true;
+                  _switchesStateMap['TOAST'] = true;
+                  //  _switchLocalPushEnabled = false;
+                  _switchesStateMap['LOCALPUSH'] = false;
+                }
+                break;
+              case 'LOCALPUSH':
+                {
+                  // _switchDialogEnabled = false;
+                  _switchesStateMap['DIALOG'] = false;
+                  //  _SwitchToastEnabled = false;
+                  _switchesStateMap['TOAST'] = false;
+                  // _switchLocalPushEnabled = true;
+                  _switchesStateMap['LOCALPUSH'] = true;
+                }
+            }
+            widget.onSwitchStateChange(_switchesStateMap);
+          } else {
+            switch (type) {
+              case 'DIALOG':
+                {
+                  // _switchDialogEnabled = false;
+                  _switchesStateMap['DIALOG'] = false;
+                }
+                break;
+              case 'TOAST':
+                {
+                  //  _SwitchToastEnabled = false;
+                  _switchesStateMap['TOAST'] = false;
+                }
+                break;
+              case 'LOCALPUSH':
+                {
+                  // _switchLocalPushEnabled = false;
+                  _switchesStateMap['LOCALPUSH'] = false;
+                }
+            }
+            widget.onSwitchStateChange(_switchesStateMap);
+          }
+        });
+      },
     );
   }
 }
